0f059fbd0f9a9e1b3f050ed832af949b9eeb4a41
Nikita Gupta <75840623+NikitaGupta-17@users.noreply.github.com>
2023-01-11 16:02:32 +0530

    Update AboutView.vue

--- demo/src/views/AboutView.vue
+++ demo/src/views/AboutView.vue
@@ -6,9 +6,12 @@
       <strong>New</strong>
     </button>
   </div>
+  <br />
+  <br />
+  <br />
   <div class="loginPopup">
     <div class="formPopup" id="popupForm">
-      <form class="formContainer">
+      <form class="formContainer" @submit.prevent="addUserBtnClicked">
         <h2>Please Enter Details</h2>
         <label for="name"><strong>Name</strong></label>
         <input
@@ -18,6 +21,7 @@
           data-key="Name"
           placeholder="Your Name"
           name="name"
+          v-model="name"
           required
         />
         <label for="email"><strong>E-mail</strong></label>
@@ -28,6 +32,7 @@
           data-key="Email"
           placeholder="Your Email"
           name="email"
+          v-model="email"
           required
         />
         <label for="book"><strong>Book</strong></label>
@@ -38,6 +43,7 @@
           data-key="Book"
           placeholder="Enter book name"
           name="book"
+          v-model="book"
           required
         />
         <label for="author"><strong>Author</strong></label>
@@ -48,10 +54,11 @@
           data-key="uthor"
           placeholder="Author Name"
           name="author"
+          v-model="author"
           required
         />
         <!-- eslint-disable -->
-        <button type="submit" class="btn" id="save" @click="addUserBtnClicked()">
+        <button type="submit" class="btn" id="save">
           Save Details
         </button>
         <button type="button" class="btn cancel" @click="closeForm()">
@@ -60,6 +67,22 @@
       </form>
     </div>
   </div>
+  <table style="width:100%" id="ex-table">
+  <tr id="tr">
+    <th>Name</th>
+    <th>Email Id</th> 
+    <th>Book</th>
+    <th>Author</th>
+    <th>Delete</th>
+  </tr>
+  <tr v-for="item in object">
+    <td>{{ item.name }}</td>
+    <td>{{ item.email }}</td>
+    <td>{{ item.book }}</td>
+    <td>{{ item.uthor }}</td>
+    <td><button type="button" class="btn cancel" @click="Delete(item.name)">Delete</button></td>
+  </tr>
+</table> 
 </template>
 <style scoped>
 * {
@@ -70,7 +93,6 @@
   justify-content: center;
 }
 .openButton {
-  position: fixed;
   border: none;
   border-radius: 5px;
   background-color: #1c87c9;
@@ -121,6 +143,17 @@
 .formContainer .cancel {
   background-color: #cc0000;
 }
+.cancel {
+  padding: 12px 20px;
+  border: none;
+  background-color: #8ebf42;
+  color: #fff;
+  cursor: pointer;
+  width: 100%;
+  margin-bottom: 15px;
+  opacity: 0.8;
+  background-color: #cc0000;
+}
 .formContainer .btn:hover,
 .openButton:hover {
   opacity: 1;
@@ -129,8 +162,9 @@
 <script>
 /* eslint-disable */
 import { initializeApp } from "firebase/app";
-import { doc, setDoc , collection } from "firebase/firestore";
+import { doc, setDoc , collection, getDocs } from "firebase/firestore";
 import { getFirestore } from "firebase/firestore";
+import { deleteDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBI4x6cfWa7wVuS4Ba4PDqalYJXtHjiVlw",
@@ -145,12 +179,33 @@ const app = initializeApp(firebaseConfig);
 const dbs = getFirestore(app);
 
 export default {
+  data: function() {
+    return {
+      newItem: '',
+      object: [],
+    };
+  },
+  // computed: {
+  //   trackId: function() {
+  //     return this.object.length;
+  //   },
+  // },
   methods: {
     openForm() {
       document.getElementById("popupForm").style.display = "block";
     },
     closeForm() {
       document.getElementById("popupForm").style.display = "none";
+      document.getElementById("name").value="";
+      document.getElementById("email").value="";
+      document.getElementById("book").value="";
+      document.getElementById("author").value="";
+      document.getElementById("id").value="";
+    },
+    async Delete(childKey) {
+      this.object.pop(this.item);
+      await deleteDoc(doc(dbs, "db", childKey));
+      alert('row was removed');
     },
     async addUserBtnClicked() {
       const usersRef = collection(dbs,'db');
@@ -161,8 +216,23 @@ export default {
         let value = addUserInputsUI[i] .value;
         newUser[key] = value;
       }
-      await setDoc(doc(usersRef),newUser);
+      await setDoc(doc(dbs,"db",this.name),newUser);
+      console.log(newUser);
+      this.object.push({ name: this.name, email: this.email, book: this.book, uthor: this.author});
+      this.newItem ="";
+    },
+    async getItem() {
+      const usersRef = collection(dbs,'db');
+      const docSnap = await getDocs(collection(dbs,"db"));
+      docSnap.forEach((doc) => {
+        var val = doc.data();
+        this.object.push({ name: val.Name, email: val.Email, book: val.Book, uthor: val.uthor });
+        this.newItem ="";
+      });
     },
   },
+  beforeMount(){
+    this.getItem();
+  }
 };
 </script>
